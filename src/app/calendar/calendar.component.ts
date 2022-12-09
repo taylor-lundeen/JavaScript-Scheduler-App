90093172011f7256d67ce81a7918280918db4dc3
unknown <taylor19125@yahoo.com>
2022-12-09 10:22:10 -0800

    Added calender component to scheduler app

---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css']
+})
+export class CalendarComponent implements OnInit {
+
+  events: any;
+
+  constructor() {
+
+    this.events = []
+    
+  }
+
+  ngOnInit() {
+  }
+
+}
